7b63b650e97088c62201fe8a25d3d27b53ec0f81
Vincent B. Abolarin <vincentab@ha-shem.com>
2022-08-10 14:15:01 +0100

    convert to Meetup app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import Todo from "./components/Todo";
-
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master React"/>
-      <Todo text="Explore the full React course" />
+      Hello!
     </div>
   );
 }
