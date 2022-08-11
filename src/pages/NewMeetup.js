749802e1c920b3758e3e0f27b7ead21ed0a53358
Vincent B. Abolarin <vincentab@ha-shem.com>
2022-08-11 17:07:47 +0100

    connect to Firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,24 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetup = () => {
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+            "https://my-react-course-apps-default-rtdb.firebaseio.com/meetups.json",
+            {
+                method: "POST",
+                body: JSON.stringify(meetupData),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
     );
 }
