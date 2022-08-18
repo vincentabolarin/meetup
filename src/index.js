5772fef73a258259ef4af9c9343bbae55c5dee16
Vincent B. Abolarin <vincentab@ha-shem.com>
2022-08-18 17:18:37 +0100

    set up Favourites Context

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import {FavouritesContextProvider} from "./store/favourites-context";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <FavouritesContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FavouritesContextProvider>
 );
