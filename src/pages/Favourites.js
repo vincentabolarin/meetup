99ed08c8c070207a4d7cf8aa2ef9661547ab2b66
Vincent B. Abolarin <vincentab@ha-shem.com>
2022-08-25 14:09:05 +0100

    implement fallback message when there are no favourites

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,8 +1,22 @@
+import {useContext} from 'react';
+import FavouritesContext from '../store/favourites-context';
+import MeetupList from '../components/meetups/MeetupList';
+
 const Favourites = () => {
+    const favouritesCtx = useContext(FavouritesContext)
+
+    let content;
+
+    if (favouritesCtx.totalFavourites === 0) {
+        content = <p>You do not have any favourites at the moment.</p>
+    } else {
+        content = <MeetupList meetups={favouritesCtx.favourites} />
+    }
     return (
-        <div>
-            Favourites Page
-        </div>
+        <section>
+            <h1>My Favourites</h1>
+            {content}
+        </section>
     );
 }
  
